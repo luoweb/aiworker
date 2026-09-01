86e33cdf09b2df6c716efbde7ee664230a6edae1
Robin <luoweb@users.noreply.github.com>
2026-09-02 00:04:33 +0800

    custom vscode ext

---
+++ packages/abc.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+baseDir=$(cd `dirname $0`;pwd)
+cd $baseDir
+execStartTime=`date +%Y%m%d-%H:%M:%S`
+
+echo "${execStartTime} Exe Dir: $baseDir"
+xsed='sed -i'
+system=`uname`
+if [ "$system" == "Darwin" ]; then
+  echo "This is macOS"
+  xsed="sed -i .bak"
+else
+  echo "This is Linux"
+  xsed='sed -i'
+fi  
+
+echo "########## custom vscode extension ########## "
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#OPENCHAMBER#AIWORKER#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#openchamber#aiworker#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#OpenChamber#AiWorker#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#fedaykindev#roweb#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#github.com/openchamber/openchamber#roweb.cn/roweb/aiworker#g' @"
