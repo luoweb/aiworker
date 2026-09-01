651d64ba8398a3115366946b4781ec2166b07e18
Robin <luoweb@users.noreply.github.com>
2026-09-02 00:21:23 +0800

    filter replace

--- packages/abc.sh
+++ packages/abc.sh
@@ -22,3 +22,14 @@ find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -
 find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#OpenChamber#AiWorker#g' @"
 find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#fedaykindev#roweb#g' @"
 find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#github.com/openchamber/openchamber#roweb.cn/roweb/aiworker#g' @"
+
+# filter
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#aiworkerConfig#openchamberConfig#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#aiworkerEvents#openchamberEvents#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#aiworker-route#openchamber-route#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#AiWorkerLogo#OpenChamberLogo#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#AiWorkerPage#OpenChamberPage#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#AiWorkerTools#OpenChamberTools#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#AiWorkerVisual#OpenChamberVisual#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#AiWorkerWidget#OpenChamberWidget#g' @"
+find ${baseDir}/ -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.mdx" -o -name "*.md" \) -not -path "*/node_modules/*" | xargs -I@ bash -c "${xsed} -i.bak 's#AiWorkerNotifi#OpenChamberNotifi#g' @"
